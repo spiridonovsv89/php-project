e1d20d604a5314420147eb9663e65995fc93c237
spiridonovsv89 <46994574+spiridonovsv89@users.noreply.github.com>
2022-09-14 11:37:50 +0300

    Updare bd.php

--- blocks/bd.php
+++ blocks/bd.php
@@ -1,6 +1,4 @@
 <?php
-$db = mysql_connect ("localhost","admin","123456");
-mysql_select_db ("dalursite",$db);
-mysql_query('set character_set_client="utf8"');
-mysql_query('set character_set_results="utf8"');
+    $conn = new mysqli("db", "root", "root", "db");
+    $conn->set_charset('utf8');
 ?>
