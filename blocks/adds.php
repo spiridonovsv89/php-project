55fab73c6c9ea1577f37de6a2fae08349e74d186
spiridonovsv89 <spiridonovsv89@gmail.com>
2022-11-02 12:37:39 +0300

    Edit adds.php

--- blocks/adds.php
+++ blocks/adds.php
@@ -1,2 +1,3 @@
+<a href="book.php"><img border="0" align="right" src="img/fav.GIF" alt="Добавить в избранное"></a>
+<a href="contact.php"><img border="0" align="right" src="img/mail.gif" alt="Связаться с администрацией"></a>
 <a href="index.php"><img border="0" align="right" src="img/home.GIF" alt="Вернуться на главную страницу"></a>
-<a href="contact.php"><img border="0" align="right" src="img/mail.GIF" alt="Связаться с администрацией"></a>
