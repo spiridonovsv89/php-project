d11af6a839eff67566f17eb0afdb764b740cfa09
spiridonovsv89 <spiridonovsv89@gmail.com>
2022-11-02 12:37:09 +0300

    Edit index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-﻿<?php include("blocks/bd.php");?>
+<?php include("blocks/bd.php");?>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -20,78 +20,31 @@ body,td,th {
       <tr>
           <?php include ("blocks/lefttd.php"); ?>
         <td valign="top">
-		
-        
-        <?
-		$result77 = mysql_query("SELECT str FROM options", $db);
-$myrow77 = mysql_fetch_array($result77);
-$num = $myrow77["str"];
-// Извлекаем из URL текущую страницу
-@$page = $_GET['page'];
-// Определяем общее число сообщений в базе данных
-$result00 = mysql_query("SELECT COUNT(*) FROM news");
-$temp = mysql_fetch_array($result00);
-$posts = $temp[0];
-// Находим общее число страниц
-$total = (($posts - 1) / $num) + 1;
-$total =  intval($total);
-// Определяем начало сообщений для текущей страницы
-$page = intval($page);
-// Если значение $page меньше единицы или отрицательно
-// переходим на первую страницу
-// А если слишком большое, то переходим на последнюю
-if(empty($page) or $page < 0) $page = 1;
-  if($page > $total) $page = $total;
-// Вычисляем начиная с какого номера
-// следует выводить сообщения
-$start = $page * $num - $num;
-// Выбираем $num сообщений начиная с номера $start
-		
-		$result = mysql_query("SELECT news.title,news.date,news.text,departments.name FROM news, departments WHERE news.dep = departments.id ORDER BY date DESC, news.id DESC LIMIT $start, $num",$db);
-		$myrow = mysql_fetch_array($result);
-		
-		
-	do {	
-printf ("<table align='center' class='news'>
-  <tr>
-    <td class='news_title'><p class='news_name'>%s</p>
-	<p class='news_date'>Новость добавлена: %s</p>
-	<p class='news_date'>Размещено для: %s</p></td>
-  </tr>
-  <tr>
-    <td>%s</td>
-  </tr>
-</table><br><br>",$myrow["title"],$myrow["date"],$myrow["name"],$myrow["text"]);} 
-while ($myrow = mysql_fetch_array($result));
-// Проверяем нужны ли стрелки назад
-if ($page != 1) $pervpage = '<a href=index.php?page=1>Первая</a> | <a href=index.php?page='. ($page - 1) .'>Предыдущая</a> | ';
-// Проверяем нужны ли стрелки вперед
-if ($page != $total) $nextpage = ' | <a href=index.php?page='. ($page + 1) .'>Следующая</a> | <a href=index.php?page=' .$total. '>Последняя</a>';
+          <?
 
-// Находим две ближайшие станицы с обоих краев, если они есть
-if($page - 5 > 0) $page5left = ' <a href=index.php?page='. ($page - 5) .'>'. ($page - 5) .'</a> | ';
-if($page - 4 > 0) $page4left = ' <a href=index.php?page='. ($page - 4) .'>'. ($page - 4) .'</a> | ';
-if($page - 3 > 0) $page3left = ' <a href=index.php?page='. ($page - 3) .'>'. ($page - 3) .'</a> | ';
-if($page - 2 > 0) $page2left = ' <a href=index.php?page='. ($page - 2) .'>'. ($page - 2) .'</a> | ';
-if($page - 1 > 0) $page1left = '<a href=index.php?page='. ($page - 1) .'>'. ($page - 1) .'</a> | ';
+          $sql = "select * from news";
+          $result = $conn->query($sql);
 
-if($page + 5 <= $total) $page5right = ' | <a href=index.php?page='. ($page + 5) .'>'. ($page + 5) .'</a>';
-if($page + 4 <= $total) $page4right = ' | <a href=index.php?page='. ($page + 4) .'>'. ($page + 4) .'</a>';
-if($page + 3 <= $total) $page3right = ' | <a href=index.php?page='. ($page + 3) .'>'. ($page + 3) .'</a>';
-if($page + 2 <= $total) $page2right = ' | <a href=index.php?page='. ($page + 2) .'>'. ($page + 2) .'</a>';
-if($page + 1 <= $total) $page1right = ' | <a href=index.php?page='. ($page + 1) .'>'. ($page + 1) .'</a>';
+          while($row = $result->fetch_assoc() ){
 
-// Вывод меню если страниц больше одной
+        echo "<table align='center' class='news'>";
+        echo "<tr>";
+        echo "<td class='news_title'><p class='news_name'>". $row["title"] ."</p>";
+	      echo "<p class='news_date'>Новость добавлена: " . $row["date"] . "</p>";
+        echo "</tr>";
+        echo "<tr>";
+        echo "<td>" . $row["text"] . "</td>";
+        echo "</tr>";
+        echo "</table><br>";
 
-if ($total > 1)
-{
-Error_Reporting(E_ALL & ~E_NOTICE);
-echo "<div align=\"center\" class=\"pstrnav\">";
-echo $pervpage.$page5left.$page4left.$page3left.$page2left.$page1left.'<b>'.$page.'</b>'.$page1right.$page2right.$page3right.$page4right.$page5right.$nextpage;
-echo "</div>";
-}
-      ?>
-        
+                              }
+
+
+
+        $conn->close();
+
+
+          ?>
         </td>
         </tr>
     </table></td>
