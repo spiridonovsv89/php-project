47f4dbd0097140a46a0d220a4d9332f1a4f6d226
spiridonovsv89 <spiridonovsv89@gmail.com>
2022-11-02 12:37:30 +0300

    Edit lefttd.php

--- blocks/lefttd.php
+++ blocks/lefttd.php
@@ -1,68 +1,52 @@
-﻿<td width="184" valign="top" class="left">
+<td width="184" valign="top" class="left">
 <div class="menu_title">Главное меню</div>
 <div id="coolmenu">
 <a href="index.php">Новости</a>
 <a href="info.php">Информация</a>
 <a href="files.php">Файлы</a>
-<a href="new_bid.php">Техническая поддержка</a>
-<a href="admin/index.php">Администраторский блок</a>
+</div>
+<div class="menu_title">Отделы</div>
+<div id="coolmenu">
+
+<?
+
+$sql_dep = "select * from departments";
+$result_dep = $conn->query($sql_dep);
+if ($result_dep->num_rows > 0){
+while($row = $result_dep->fetch_assoc() ){
+      echo "<a href=''>" .  $row["title"] . "</a>";
+}
+} else {
+      echo "0 records";
+}
+
+?>
+
 </div>
 <div class="menu_title">Корпоративные ссылки</div>
 <div id="coolmenu">
+
 <?
-mysql_query('set character_set_client="utf8"');
-mysql_query('set character_set_results="utf8"');
-$result_links = mysql_query("SELECT title,link FROM links",$db);
-if (!$result_links) {
-echo "<p>Извлечь данные из базы не удалось.<br></p>";
-exit(mysql_error());
+
+$sql_link = "select * from links";
+$result_link = $conn->query($sql_link);
+if ($result_link->num_rows > 0){
+while($row = $result_link->fetch_assoc() ){
+      echo "<a href='" . $row["link"] . "'>" .  $row["title"] . "</a>";
 }
-
-if (mysql_num_rows($result_links) > 0) {
-	$myrow_links = mysql_fetch_array($result_links);
-	
-	do {	
-printf ("<a href='%s' target='_blank'>%s</a>",$myrow_links["link"],$myrow_links["title"]);
-} 
-while ($myrow_links = mysql_fetch_array($result_links));
-	}
-	
-	else {
-		echo "<p>Извлечь данные из базы не удалось, в таблице нет записей.</p>";
-		exit();
-	}
-
-?></div>
-<div class="menu_title">Отделы</div>
-<div id="coolmenu">
-<?
-$result_dep = mysql_query("SELECT * FROM departments WHERE NOT id=1",$db);
-if (!$result_dep) {
-echo "<p>Извлечь данные из базы не удалось.<br></p>";
-exit(mysql_error());
+} else {
+      echo "0 records";
 }
-
-if (mysql_num_rows($result_dep) > 0) {
-	$myrow_dep = mysql_fetch_array($result_dep);
-	
-	do {	
-printf ("<a href='view_dep.php?dep=%s'>%s</a>",$myrow_dep["id"],$myrow_dep["name"]);
-} 
-while ($myrow_dep = mysql_fetch_array($result_dep));
-	}
-	
-	else {
-		echo "<p>Извлечь данные из базы не удалось, в таблице нет записей.</p>";
-		exit();
-	}
 
 ?>
+
+
 </div>
 <div class="menu_title">Поиск</div>
 <div id="coolmenu">
 <form class="bottom" action="view_search.php" method="post" name="form_s">
 <p class="search_text">Введите данные для поиска:</p><br>
 <p class="search_input"><input  type="text" size="17" maxlength="50" name="search"></p><br>
-<input class="search_button" type="submit" name="submit_s" value="Искать">  
+<input class="search_button" type="submit" name="submit_s" value="Искать">
 </form></div>
 </td>
