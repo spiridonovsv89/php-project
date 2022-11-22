90e07d296fa3ced11982783d72c43f04e8cf417d
spiridonovsv89 <spiridonovsv89@gmail.com>
2022-11-22 11:41:02 +0300

    Move terraform files to deploy folder

---
+++ deploy/main.tf
@@ -0,0 +1,49 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "docker" {
+  ami                    = "ami-097a2df4ac947655f"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.docker_sg.id]
+  key_name               = "terraform"
+  user_data = templatefile("script.bash", {
+    ComposePath = var.path
+  })
+  tags = {
+    Name    = "Docker Instance"
+    Project = "php-project"
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "docker_sg" {
+  name = "Docker Security Group"
+
+  dynamic "ingress" {
+    for_each = var.ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "php-project-bucket"
+    key    = "terraform.tfstate"
+    region = "us-east-2"
+  }
+}
